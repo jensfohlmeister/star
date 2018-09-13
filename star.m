0425f564fb23dd0700e5f43958fc45af3ce5f23f
jensfohlmeister <42670538+jensfohlmeister@users.noreply.github.com>
2018-09-13 09:39:50 +0200

    Update star.m

--- star.m
+++ star.m
@@ -8,11 +8,30 @@
 %The input file should contain the following columns :
 %Depth from top, measured fm, error in fm.
 
-%%%%COPYRIGHT%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Copyr ight ( c ) 2016 :
-% Franziska Lechleitner (1,2), Jens Fohlmeister (3) , James U.L. Baldini (2)
-% (1) ETH Zurich, Switzerland, (2) Durham University , UK, (3) University of
-% Heidelberg, Germany
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%STAlagmite dating by Radiocarbon (star): a software tool for reliable and fast age depth modelling
+%
+%The software comes along with the following publication: Fohlmeister, J., Lechleitner, F.A., 
+%STAlagmite dating by Radiocarbon (star): a software tool for reliable and fast age depth 
+%modelling, Quaternary Geochronology, submitted
+%
+%based on previous work, presented in:
+%
+%Lechleitner, F. A., Fohlmeister, J., McIntyre, C., Baldini, L., Jamieson, R. A., Hercman, H., 
+%Gasiorowski, M., Pawlak, J., Stefaniak, K., Socha, P., Eglinton, T. I., Baldini, J. U. L., 2016. 
+%A novel approach for reconstruction of accurate radiocarbon-based chronologies for speleothems. 
+%Quaternary Geochronology 35, 54-66.
+%
+%Please cite the paper(s) if you are using this fantastic software. ;)
+%
+%Please read the paper before considering this software to construct an age-depth model for your climate archive.
+%
+%A one-page manual is provided for a quick start. Please read this. Afterwards you should be able 
+%to handle the most important features of 'star' and application of your dataset shouldn't be a problem anymore.
+%
+%Star is open-source software; you are free to use, copy, distribute and modify it. However, 
+%it does not come with any warranty and the authors do not assume any responsibility for the 
+%usefulness of any portion of this program.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clear
